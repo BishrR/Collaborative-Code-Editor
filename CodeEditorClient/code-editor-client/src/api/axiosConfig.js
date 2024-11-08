2ca8088c203304f427913c944df5200ae99f6048
BishrR <bishr.alqudah@gmail.com>
2024-11-08 18:26:04 +0400

    Update axiosConfig.js

--- CodeEditorClient/code-editor-client/src/api/axiosConfig.js
+++ CodeEditorClient/code-editor-client/src/api/axiosConfig.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export default axios.create({
     // baseURL: 'http://localhost:8080/',
-    baseURL: 'http://b4r-c11e-c2e-e4r-m4r-931744921.us-east-1.elb.amazonaws.com:8080'
+    baseURL: 'http://b4r-c11e-c2e-e4r-m4r-2052193443.us-east-1.elb.amazonaws.com'
     // headers: {"ngrok-skip-browser-warning": "true"}
 });
