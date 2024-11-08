2b12caa952a5906c366927f3e04213850b3c19b2
BishrR <bishr.alqudah@gmail.com>
2024-11-08 22:11:41 +0400

    Update axiosConfig.js

--- CodeEditorClient/code-editor-client/src/api/axiosConfig.js
+++ CodeEditorClient/code-editor-client/src/api/axiosConfig.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const ec2PublicIp = process.env.EC2_PUBLIC_IP;
+
 export default axios.create({
     // baseURL: 'http://localhost:8080/',
-    baseURL: 'http://code-editor:8080'
+    baseURL: 'http://' + ec2PublicIp + ':8080'
     // headers: {"ngrok-skip-browser-warning": "true"}
 });
